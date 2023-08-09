90e4701921cc1d79d8f1f0a47d8458d8ce58bdff
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-08-09 10:35:20 +0200

    Mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,31 @@
 import React from "react";
 import { nanoid } from 'nanoid';
-import { Form } from './Form/Form';
+import Form  from './Form/Form';
 import PropTypes from 'prop-types';
 import { Filter } from './Filter/Filter';
 import ListContacts from './ContactList/ContactList';
 import ContactsItem from "./ContactsItem/ContactsItem";
 import css from './App.module.css';
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      contacts: [],
-      name: '',
-    };
-  }
-
-  handleAddContact = (name, number) => {
-    const { contacts } = this.state;
-    const isContactExist = contacts.some(contact => contact.name === name);
-    
-    if (isContactExist) {
-      alert(`${name} is already in contacts.`);
-    } else {
-      const contactItem = {
-        name,
-        id: nanoid(),
-        number,
-      };
-      
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, contactItem]
-      }));
-    }
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+    filter: '',
   };
+
+  handleAddContact = ({name, number}) => {
+     const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.checkNameToSame(name)
+      ? alert(`${name} is already in contacts`)
+      : this.addNewContact(newContact);
+  };
+
 
   checkNameToSame = name => {
     const lowerCaseNewName = name.toLowerCase();
@@ -50,6 +41,12 @@ export class App extends React.Component {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
   };
 
   render() {
@@ -80,7 +77,9 @@ App.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-      filter: PropTypes.string.isRequired,
+      // filter: PropTypes.string.isRequired,
     })
-  ).isRequired,
+  ),
 };
+
+export default App;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,61 +2,60 @@ import React from "react";
 import PropTypes from 'prop-types';
 import css from './Form.module.css';
 
-export class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            number: '',
-        };
-    }
-   
-
-    handleChange = evt => {
-        this.setState({ [evt.target.name]: evt.target.value });
+class Form extends React.Component {
+    state = {
+        name: '',
+        number: '',
+    };
+    handleChange = e => {
+        const { name, value } = e.currentTarget;
+        this.setState({ [name]: value });
+    };
+    handleFormSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state);
+        this.reset();
+    };
+    reset = () => {
+        this.setState({ name: '', number: '' });
     };
 
-    handleSubmit = evt => {
-        evt.preventDefault();
-        
-        const { name, number } = this.state;
-
-        this.props.onAddContact(name, number);
-        this.setState({ name: '', number: '' })
-    };
 
     render() {
         const { name, number } = this.state;
         return (
-            <form onSubmit={this.handleSubmit} className={css.form}>
+            <form onSubmit={this.handleFormSubmit} className={css.form}>
                 <label className={css.label}><span className={css.span}>Name</span>
-                <input className={css.input}
-                    type="text"
-                    value={name}
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    onChange={this.handleChange}
+                    <input className={css.input}
+                        type="text"
+                        value={name}
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        onChange={this.handleChange}
                     />
-                </label>    
+                </label>
                 <label className={css.label}><span className={css.span}>Number</span>
-                <input className={css.inputNumber}
-                    type="tel"
-                    value={number}
-                    name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    onChange={this.handleChange}
+                    <input className={css.inputNumber}
+                        type="tel"
+                        value={number}
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                        onChange={this.handleChange}
                     />
                 </label>
                 <button type="submit" className={css.button}>Add contact</button>
             </form>
         );
     }
-
-    static propTypes = {
-        onAddContact: PropTypes.func.isRequired,
-    }
+}
+Form.propTypes = {
+        handleFormSubmit: PropTypes.func.isRequired,
+        onSubmit: PropTypes.func.isRequired,
+        handleChange: PropTypes.func.isRequired,
 };
+    
+export default Form;
