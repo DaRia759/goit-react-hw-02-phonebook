2ec09d37985963ce101b049813e82f5b857a0da4
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-08-09 11:11:31 +0200

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -22,13 +22,12 @@ class Form extends React.Component {
 
 
     render() {
-        const { name, number } = this.state;
         return (
             <form onSubmit={this.handleFormSubmit} className={css.form}>
                 <label className={css.label}><span className={css.span}>Name</span>
                     <input className={css.input}
                         type="text"
-                        value={name}
+                        value={this.state.name}
                         name="name"
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -39,7 +38,7 @@ class Form extends React.Component {
                 <label className={css.label}><span className={css.span}>Number</span>
                     <input className={css.inputNumber}
                         type="tel"
-                        value={number}
+                        value={this.state.number}
                         name="number"
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
@@ -52,10 +51,11 @@ class Form extends React.Component {
         );
     }
 }
+
 Form.propTypes = {
-        handleFormSubmit: PropTypes.func.isRequired,
-        onSubmit: PropTypes.func.isRequired,
-        handleChange: PropTypes.func.isRequired,
+    handleFormSubmit: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
 };
     
 export default Form;
